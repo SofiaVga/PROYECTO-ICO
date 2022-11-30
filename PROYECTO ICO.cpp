685a239895eede1ec7035d6b063ff9f8eb9f1c54
SofiaVga <sofiavegagomez04@gmail.com>
2022-11-30 15:44:12 -0600

    Primer version de CYP

---
+++ PROYECTO ICO.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int dia, mes, anio;
+	printf("Ingrese el dia: "); scanf_s("%d", &dia);
+	printf("Ingrese el mes: "); scanf_s("%d", &mes);
+	printf("Ingrese el anio: "); scanf_s("%d", &anio);
+}
